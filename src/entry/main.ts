da97e705d4dae9d49d772a71ff266b2e8c0ec74a
ADRIAN GROPPER <agropper@healthurl.com>
2025-08-07 13:23:06 -0400

    Fix CSS loading order and implement real PDF parsing with pdf-parse

--- src/entry/main.ts
+++ src/entry/main.ts
@@ -1,7 +1,7 @@
-import '@/css/main.scss'
-import '@/css/quasar.variables.sass'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
+import '@/css/quasar.variables.sass'
+import '@/css/main.scss'
 
 import App from '@/entry/App.vue'
 import { Quasar } from 'quasar'
